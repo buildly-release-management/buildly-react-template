5fc164b8f42dca15ed96061552c587331913a91a
Siddharth Bhansali <siddharth.bhansali@ajackus.com>
2022-04-20 15:26:51 +0530

    Adding Google Ad Tag.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,28 @@ import App from './App';
 import './i18n';
 import configureStore from './redux/store';
 
+if(window.env.PRODUCTION) {
+    const srcUrl = "https://www.googletagmanager.com/gtag/js?id=";
+    const id = 'UA-226504961-1';
+
+    const script = document.createElement('script');
+    script.src = `${srcUrl}${id}`;
+    script.async = true;
+
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag() { dataLayer.push(arguments); }
+                            gtag('js', new Date());
+                            gtag('config', ${id}, { 'send_page_view': false });
+                        `;
+
+    document.body.appendChild(script);
+    document.body.appendChild(script2);
+}
+
 const store = configureStore();
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
