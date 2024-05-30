1ad717e66b8c6228eecbc8fc9c7bcb7c07a8510b
Radhika Patel <radhika.patel2206@gmail.com>
2024-05-30 23:27:28 +0530

    fix window env setup

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import registerServiceWorker from './serviceWorkerRegistration';
 
+fetch('/environment.json').then((response) => response.json()).then((env) => { window.env = env; });
+
 if (window.env && window.env.PRODUCTION) {
   const script = document.createElement('script');
   script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-145772893-1';
