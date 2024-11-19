c17c0e3a7ad287e7cec749fefe565b4d628052cb
Radhika Patel <radhika.patel2206@gmail.com>
2024-11-19 11:12:23 +0530

    Merge master

--- src/pages/ReleaseDetails/components/ReleaseForm.js
+++ src/pages/ReleaseDetails/components/ReleaseForm.js
@@ -1,27 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
-import { Button } from '@mui/material';
+import {
+  Autocomplete, Button, Checkbox, TextField,
+} from '@mui/material';
 import Loader from '@components/Loader/Loader';
 import { useUpdateReleaseMutation } from '@react-query/mutations/release/updateReleaseMutation';
 import './ReleaseForm.css';
+import _ from 'lodash';
+import { useQuery } from 'react-query';
+import { getAllFeatureQuery } from '@react-query/queries/release/getAllFeatureQuery';
+import { CheckBox as CheckBoxIcon, CheckBoxOutlineBlank as CheckBoxOutlineBlankIcon } from '@mui/icons-material';
 
 const ReleaseForm = ({ releasesDetails, displayAlert }) => {
   const [formData, setFormData] = useState({});
+  const [selectedFeatures, setSelectedFeatures] = useState([]);
 
-  const { mutate: updateReleaseMutation, isLoading: isUpdatingReleaseLoading } = useUpdateReleaseMutation(releasesDetails.release_uuid, displayAlert);
+  const uncheckedIcon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+  const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
+  const { data: features, isLoading: isAllFeatureLoading } = useQuery(
+    ['allFeatures', releasesDetails?.product_uuid],
+    () => getAllFeatureQuery(releasesDetails?.product_uuid, displayAlert),
+    { refetchOnWindowFocus: false, enabled: !_.isEmpty(releasesDetails?.product_uuid) && !_.isEqual(_.toNumber(releasesDetails?.product_uuid), 0) },
+  );
+
+  // Initialize selected features
   useEffect(() => {
-    setFormData({});
-    if (releasesDetails) {
-      setFormData({
-        ...formData,
-        release_uuid: releasesDetails.release_uuid,
-        name: releasesDetails.name,
-        description: releasesDetails.description,
-        release_date: releasesDetails.release_date,
-      });
+    resetForm();
+    if (!_.isEmpty(features)) {
+      const selected = features.filter((featureObj) => releasesDetails.features.includes(featureObj.feature_uuid));
+      setSelectedFeatures(selected);
     }
-  }, []);
+  }, [features]);
+
+  const { mutate: updateReleaseMutation, isLoading: isUpdatingReleaseLoading } = useUpdateReleaseMutation(releasesDetails?.release_uuid, displayAlert);
 
   const updateFormData = (e) => {
     setFormData({
@@ -29,7 +41,7 @@ const ReleaseForm = ({ releasesDetails, displayAlert }) => {
       [e.target.name]: e.target.value,
     });
   };
-  console.log(formData);
+
   const submitRelease = (event) => {
     event.preventDefault();
     updateReleaseMutation(formData);
@@ -38,18 +50,19 @@ const ReleaseForm = ({ releasesDetails, displayAlert }) => {
   const resetForm = () => {
     setFormData({
       ...formData,
-      release_uuid: releasesDetails.release_uuid,
-      name: releasesDetails.name,
-      description: releasesDetails.description,
-      release_date: releasesDetails.release_date,
+      release_uuid: releasesDetails?.release_uuid,
+      name: releasesDetails?.name,
+      description: releasesDetails?.description,
+      release_date: releasesDetails?.release_date,
+      features: releasesDetails?.features,
     });
   };
 
   return (
     <>
-      {isUpdatingReleaseLoading && <Loader open={isUpdatingReleaseLoading} />}
+      {(isUpdatingReleaseLoading || isAllFeatureLoading) && <Loader open={isUpdatingReleaseLoading || isAllFeatureLoading} />}
       <Form noValidate>
-        <Form.Group className="mb-3 col-md-6 col-sm-12" controlId="name">
+        <Form.Group className="mb-3" controlId="name">
           <Form.Label>Name*</Form.Label>
           <Form.Control
             size="sm"
@@ -71,7 +84,7 @@ const ReleaseForm = ({ releasesDetails, displayAlert }) => {
             onChange={(event) => updateFormData(event)}
           />
         </Form.Group>
-        <Form.Group className="mb-3 col-md-3 col-sm-12" controlId="date">
+        <Form.Group className="mb-3 col-md-6 col-sm-12" controlId="date">
           <Form.Label>Release date*</Form.Label>
           <Form.Control
             size="sm"
@@ -81,6 +94,43 @@ const ReleaseForm = ({ releasesDetails, displayAlert }) => {
             value={formData.release_date}
             required
             onChange={(event) => updateFormData(event)}
+          />
+        </Form.Group>
+        <Form.Group
+          className="mb-3"
+          controlId="features"
+        >
+          <Form.Label>Add Features</Form.Label>
+          <Autocomplete
+            multiple
+            id="features"
+            disableCloseOnSelect
+            filterSelectedOptions
+            options={_.orderBy(features, ['name'], ['asc'])}
+            getOptionLabel={(option) => option && option.name}
+            value={selectedFeatures}
+            onChange={(event, newValue) => {
+              if (_.size(newValue) > _.size(selectedFeatures)) {
+                setSelectedFeatures([
+                  ...selectedFeatures,
+                  _.last(newValue),
+                ]);
+              } else {
+                setSelectedFeatures(newValue);
+              }
+            }}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={uncheckedIcon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option.name}
+              </li>
+            )}
+            renderInput={(params) => <TextField {...params} />}
           />
         </Form.Group>
       </Form>
