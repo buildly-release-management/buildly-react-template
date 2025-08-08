217832037a448f5e866f1d2f55f3567952877cd3
Greg Lind <gwlind@gmail.com>
2025-08-07 17:00:37 -0700

    Update landing page to show Buildly onboarding
    - Replace Google Storage iframe with labs-onboarding.buildly.io
    - Remove deprecated MUI makeStyles usage
    - Update iframe title to reflect onboarding content

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import makeStyles from '@mui/styles/makeStyles';
 import Copyright from '@components/Copyright/Copyright';
 
-const useStyles = makeStyles((theme) => ({
-  iframe: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    border: 'none',
-  },
-}));
-
 const Home = () => {
-  const classes = useStyles();
-
   return (
     <>
       <iframe
-        title="Buildly Product Labs Home Page"
+        title="Buildly Product Labs Onboarding"
         loading="lazy"
-        className={classes.iframe}
-        src={`https://storage.googleapis.com/insights-landing/index.html?origin=https://${window.env.HOSTNAME}/`}
+        style={{ position: 'absolute', width: '100%', height: '100%', border: 'none' }}
+        src="https://labs-onboarding.buildly.io"
         allowFullScreen
       />
       <Copyright />
